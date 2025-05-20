4303c2cc6dc555be2ab5a7884abcbde8c4b92ddd
michalwietecki <132830351+michalwietecki@users.noreply.github.com>
2025-05-20 20:59:52 +0300

    changed soft labels to always be 0.9999

--- augmentation_generator_batch_size.py
+++ augmentation_generator_batch_size.py
@@ -64,7 +64,7 @@ class AugmentedCIFAR10Generator(Sequence):
             img_pil = Image.fromarray(img)
             img_aug, label_aug = self.augmentor(img_pil, label.copy())
 
-            # convert the soft label to that it not dependent on the area
+            # convert the soft label so that it's not dependent on the area
             label_aug = [0 if k==0 else 0.9999 for k in label_aug]
             
             x_augmented.append(np.array(img_aug))
